5ea1bc1aa985b36f2197454dc874a93a35deba24
dwayne-pinto <110647668+dwayne-pinto@users.noreply.github.com>
2022-10-17 13:35:07 +1100

    Update graphs.js

--- graphs.js
+++ graphs.js
@@ -1,5 +1,7 @@
 var vg_1 = "PPM_map.vl.json";
 var vg_2 = "bubble_chart.json";
+var vg_3 = "disease_chart.json";
 
 vegaEmbed("#ppm_map", vg_1).then(function(result) {}).catch(console.error);
 vegaEmbed("#bubble_chart", vg_2).then(function(result) {}).catch(console.error);
+vegaEmbed("#disease_chart", vg_3).then(function(result) {}).catch(console.error);
